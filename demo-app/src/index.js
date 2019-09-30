e4722b119bdfb049db17d11c4e71a8df2ef1bac7
Eric Greene <eric@t4d.io>
2019-09-30 09:31:21 -0700

    add code demos

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
 
-import { carStore } from './stores/carStore';
-import { CarToolContainer } from './containers/CarToolContainer';
+// import { carStore } from './stores/carStore';
+// import { CarToolContainer } from './containers/CarToolContainer';
 
-ReactDOM.render(
-  <Provider store={carStore}>
-    <CarToolContainer />
-  </Provider>,
-  document.querySelector('#root'),
-);
+// ReactDOM.render(
+//   <Provider store={carStore}>
+//     <CarToolContainer />
+//   </Provider>,
+//   document.querySelector('#root'),
+// );
